5b44221f40c801e499395e1b3232efe52546e612
1999single <595082586@qq.com>
2020-03-14 03:23:02 +0800

    开启Https

--- src/main/java/com/single/yourme/YourMeApplication.java
+++ src/main/java/com/single/yourme/YourMeApplication.java
@@ -33,32 +33,32 @@ public class YourMeApplication {
         application.run(args);
     }
 
-//    @Bean
-//    public ServletWebServerFactory servletContainer() {
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        tomcat.addAdditionalTomcatConnectors(httpConnector());
-//        return tomcat;
-//    }
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
 
-//    @Bean
-//    public Connector httpConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        // Connector监听的http的端口号
-//        connector.setPort(80);
-//        connector.setSecure(false);
-//        // 监听到http的端口号后转向到的https的端口号
-//        connector.setRedirectPort(443);
-//        return connector;
-//    }
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        // Connector监听的http的端口号
+        connector.setPort(80);
+        connector.setSecure(false);
+        // 监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
